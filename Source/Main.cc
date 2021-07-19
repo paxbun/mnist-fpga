4510b6b1ab097fc1ca6a64da4c4034ba69c1470b
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-07-19 13:24:25 +0900

    Add test C++ source file

---
+++ Source/Main.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Chanjung Kim. All rights reserved.
+// Licensed under the MIT License.
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
