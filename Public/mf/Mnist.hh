a5cb17082fabe05f51e5a12a59d14c9be0a278e6
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-07-20 16:50:20 +0900

    Add class Mnist

--- Public/mf/Mnist.hh
+++ Public/mf/Mnist.hh
@@ -4,10 +4,139 @@
 #ifndef MNIST_FGPA_MNIST_HH
 #define MNIST_FGPA_MNIST_HH
 
+#include <mf/Config.hh>
+#include <mf/File.hh>
+
+#include <cstdint>
+#include <filesystem>
+#include <stdexcept>
+#include <vector>
+
 namespace mf
 {
 
+/**
+ * Represents label for one single MNIST sample.
+ */
+enum class MnistLabel : uint8_t
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine
+};
 
+/**
+ * Represents one single immutable MNIST sample.
+ */
+struct MnistSample
+{
+    constexpr static size_t width { 28 };
+    constexpr static size_t height { 28 };
+
+    float const (&image)[width][height];
+    MnistLabel const label;
+};
+
+MF_MAKE_NEW_EXCEPTION(InvalidMnistDatasetException, "MNIST dataset file is corrupted");
+
+MF_MAKE_NEW_EXCEPTION(MnistSampleNumberDoesNotMatchException,
+                      "The number of images and the number of labels are different");
+
+/**
+ * Represents a collection of hand-written 28x28 greyscale digit images and corresponding labels.
+ */
+class Mnist
+{
+  public:
+    /**
+     * Reads the given two files and creates one complete MNIST dataset instance.
+     *
+     * @param imagePath the file containing image data
+     * @param labelPath the file containing label data
+     * @throws NoSuchFileException
+     * @throws InvalidMnistDatasetException
+     * @throws MnistSampleNumberDoesNotMatchException
+     */
+    static Mnist MakeFromFile(std::filesystem::path const& imagePath,
+                              std::filesystem::path const& labelPath);
+
+    /**
+     * Reads two MNIST data files specified in the config and creates one complete MNIST dataset
+     * instance.
+     *
+     * @param config the configuration
+     * @throws NoSuchFileException
+     * @throws InvalidMnistDatasetException
+     * @throws MnistSampleNumberDoesNotMatchException
+     */
+    inline static Mnist MakeFromFile(Config const& config)
+    {
+        return MakeFromFile(config.mnistImageFilePath, config.mnistLabelFilePath);
+    }
+
+  private:
+    size_t                  _numSamples;
+    std::vector<float>      _images;
+    std::vector<MnistLabel> _labels;
+
+  private:
+    Mnist(size_t numSamples, std::vector<float>&& images, std::vector<MnistLabel>&& labels) :
+        _numSamples { numSamples },
+        _images { std::move(images) },
+        _labels { std::move(labels) }
+    {}
+
+  public:
+    /**
+     * Returns a `MnistSample` instance corresponding to the given index.
+     *
+     * @param idx the index of the sample
+     * @throws std::invalid_argument if the index is equal to or greater than the number of samples
+     */
+    MnistSample GetSample(size_t idx) const
+    {
+        if (idx >= _numSamples)
+            throw std::invalid_argument { "idx" };
+
+        return MnistSample {
+            ((float(*)[MnistSample::width][MnistSample::height])_images.data())[idx],
+            _labels[idx],
+        };
+    }
+
+    /**
+     * Returns the internal buffer containing image data. The length of the vector is 28 x 28 x
+     * `GetNumberSamples()`.
+     */
+    std::vector<float> const& GetImages() const noexcept
+    {
+        return _images;
+    }
+
+    /**
+     * Returns the internal buffer containing label data. The length of the vector is
+     * `GetNumberSamples()`.
+     */
+    std::vector<MnistLabel> const& GetLabels() const noexcept
+    {
+        return _labels;
+    }
+
+    /**
+     * Returns the number of samples.
+     */
+    size_t GetNumSamples() const noexcept
+    {
+        return _numSamples;
+    }
+};
 
 }
 
---
+++ Source/Mnist.cc
@@ -0,0 +1,124 @@
+// Copyright (c) 2021 Chanjung Kim. All rights reserved.
+// Licensed under the MIT License.
+
+#include <mf/File.hh>
+#include <mf/Mnist.hh>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define READ_FROM_IFS(expr) (ifs.read((char*)(&(expr)), sizeof(expr)))
+
+namespace mf
+{
+
+namespace
+{
+
+/**
+ * Reverts the endianness of the given integer.
+ *
+ * @param value the integer of which the endianness will be reverted.
+ */
+void RevertEndian(uint32_t& value)
+{
+    union
+    {
+        uint8_t  b[4];
+        uint32_t i;
+    } u, v;
+    u.i    = value;
+    v.b[0] = u.b[3];
+    v.b[1] = u.b[2];
+    v.b[2] = u.b[1];
+    v.b[3] = u.b[0];
+    value  = v.i;
+}
+
+/**
+ * Reads image data from the given file.
+ *
+ * @param imagePath the file to read
+ */
+std::vector<float> ReadImages(std::filesystem::path const& imagePath)
+{
+    std::ifstream ifs { imagePath, std::ifstream::binary };
+    if (!ifs)
+        throw NoSuchFileException { imagePath.string() };
+
+    uint32_t imageMagic;
+    READ_FROM_IFS(imageMagic);
+    if (imageMagic != 0x03080000)
+        throw InvalidMnistDatasetException { imagePath.string() };
+
+    uint32_t numImages;
+    READ_FROM_IFS(numImages);
+    RevertEndian(numImages);
+
+    uint32_t imageHeight, imageWidth;
+    READ_FROM_IFS(imageHeight);
+    READ_FROM_IFS(imageWidth);
+
+    if (imageHeight != MnistSample::height || imageWidth != MnistSample::width)
+        throw InvalidMnistDatasetException { imagePath.string() };
+
+    uint8_t buffer[MnistSample::height * MnistSample::width];
+    float   values[MnistSample::height * MnistSample::width];
+
+    std::vector<float> data;
+    data.reserve(numImages * MnistSample::height * MnistSample::width);
+    for (uint32_t i = 0; i < numImages; ++i)
+    {
+        READ_FROM_IFS(buffer);
+
+        for (size_t j = 0; j < MnistSample::height * MnistSample::width; ++j)
+            values[j] = buffer[j] / 255.0f;
+
+        data.insert(std::end(data), std::begin(values), std::end(values));
+    }
+
+    return data;
+}
+
+/**
+ * Reads label data from the given file.
+ *
+ * @param labelPath the file to read
+ */
+std::vector<MnistLabel> ReadLabels(std::filesystem::path const& labelPath)
+{
+    std::ifstream ifs { labelPath, std::ifstream::binary };
+    if (!ifs)
+        throw NoSuchFileException { labelPath.string() };
+
+    uint32_t labelMagic;
+    READ_FROM_IFS(labelMagic);
+    if (labelMagic != 0x01080000)
+        throw InvalidMnistDatasetException { labelPath.string() };
+
+    uint32_t numLabels;
+    READ_FROM_IFS(numLabels);
+    RevertEndian(numLabels);
+
+    std::vector<MnistLabel> data;
+    data.resize(numLabels);
+    ifs.read((char*)data.data(), numLabels * sizeof(MnistLabel));
+
+    return data;
+}
+
+}
+
+Mnist Mnist::MakeFromFile(std::filesystem::path const& imagePath,
+                          std::filesystem::path const& labelPath)
+{
+    auto images { ReadImages(imagePath) };
+    auto labels { ReadLabels(labelPath) };
+    if (images.size() != labels.size())
+        throw MnistSampleNumberDoesNotMatchException {};
+
+    return Mnist { images.size(), std::move(images), std::move(labels) };
+}
+
+}
